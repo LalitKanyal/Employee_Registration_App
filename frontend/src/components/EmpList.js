20222f59f15387c6d0f950e8377d9ebccee78731
Lalit Kanyal <mailtolalit21@gmail.com>
2023-06-09 09:22:26 +0530

    complete

--- frontend/src/components/EmpList.js
+++ frontend/src/components/EmpList.js
@@ -1,58 +1,73 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-class EmpList extends React.Component{
-  constructor(){
-    super();
-    this.state={
-      data:[]
-    };
-  }
-  fetchData(){
-    fetch('http://localhost:8000/employee/')
-    .then(response=>response.json())
-    .then((data)=>{
-      this.setState({
-        data:data
-      })
-      // console.log("data>>>>>>", data)
-    })
-  }
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-  componentDidMount(){
-    this.fetchData();
-  }
-  render() {
-    const empData=this.state.data;
-    const rows=empData.map((emp)=>
-      <tr key={emp.id}>
-        <td>{emp.full_name}</td>
-        <td>{emp.contact}</td>
-        <td>{emp.email}</td>
-        <td>{emp.address}</td>
-        <td>
-          <Link to={"/update/"+emp.id} className="btn btn-primary mr-2">Update</Link>
-          <button className='btn btn-danger'>Delete</button>
-        </td>
-      </tr>
-    );
-    return (  
-    <table className="table table-bordered">
-    <thead className="thead-light">
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Full Name</th>
-        <th scope="col">Contact</th>
-        <th scope="col">Email</th>
-        <th scope="col">Address</th>
-        <th scope="col">Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      {rows}
-    </tbody>
-  </table>
-  )
-  }
+class EmpList extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            data:[]
+        };
+    }
+
+    fetchData(){
+        fetch('http://127.0.0.1:8000/employee/')
+        .then(response=>response.json())
+        .then((data)=>{
+            this.setState({
+                data:data
+            });
+        });
+    }
+
+    componentDidMount(){
+        this.fetchData();
+    }
+
+    deleteData(id){
+        fetch('http://127.0.0.1:8000/employee/'+id+'/',{
+            method:'DELETE',
+            body:JSON.stringify(this.state),
+        })
+        .then(response=>response)
+        .then((data)=>{
+            if(data){
+                this.fetchData();
+            }
+        });
+    }
+
+    render(){
+        const empData=this.state.data;
+        const rows=empData.map((emp)=>
+            <tr key={emp.id}>
+                <td>{emp.full_name}</td>
+                <td>{emp.email}</td>
+                <td>{emp.contact}</td>
+                <td>{emp.address}</td>
+                <td>
+                    <Link to={'update/'+emp.id} className="btn btn-info mr-2">Update</Link>
+                    <button onClick={()=>this.deleteData(emp.id)} className="btn btn-danger">Delete</button>
+                </td>
+            </tr>
+        );
+        return (
+            <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Full Name</th>
+                        <th>Email</th>
+                        <th>Contact</th>
+                        <th>Address</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+        );
+    }
+    
 }
-  
-export default EmpList
+
+export default EmpList;
--- frontend/src/components/UpdateEmp.js
+++ frontend/src/components/UpdateEmp.js
@@ -1,37 +1,115 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-export default function UpdateEmp() {
+function UpdateEmp() {
+  const [state, setState] = useState({
+    full_name: '',
+    email: '',
+    contact: '',
+    address: ''
+  });
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    fetch(`http://127.0.0.1:8000/employee/${id}`)
+      .then(response => response.json())
+      .then(data => {
+        setState({
+          full_name: data.full_name,
+          email: data.email,
+          contact: data.contact,
+          address: data.address
+        });
+      });
+  };
+
+  const changeHandler = event => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const submitForm = () => {
+    fetch(`http://127.0.0.1:8000/employee/${id}/`, {
+      method: 'PUT',
+      body: JSON.stringify(state),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    })
+      .then(response => response.json())
+      .then(data => console.log(data));
+  };
+
   return (
     <table className="table table-bordered">
-    <tr>
-    <th scope="col">Full Name</th>
-    <td>
-        <input type='text' className='form-control'/>
-    </td>
-    </tr>
-    <tr>
-    <th scope="col">Contact</th>
-    <td>
-        <input type='text' className='form-control'/>
-    </td>
-    </tr>
-    <tr>
-    <th scope="col">Email</th>
-    <td>
-        <input type='text' className='form-control'/>
-    </td>
-    </tr>
-    <tr>
-    <th scope="col">Address</th>
-    <td>
-        <input type='text' className='form-control'/>
-    </td>
-    </tr>
-    <tr>
-        <td colSpan={2}>
-        <input type='submit' className='btn btn-info'/>
-        </td>
-    </tr>
-</table>
-  )
+      <tbody>
+        <tr>
+          <th>Full Name</th>
+          <td>
+            <input
+              value={state.full_name}
+              name="full_name"
+              onChange={changeHandler}
+              type="text"
+              className="form-control"
+            />
+          </td>
+        </tr>
+        <tr>
+          <th>Email</th>
+          <td>
+            <input
+              value={state.email}
+              name="email"
+              onChange={changeHandler}
+              type="text"
+              className="form-control"
+            />
+          </td>
+        </tr>
+        <tr>
+          <th>Contact</th>
+          <td>
+            <input
+              value={state.contact}
+              name="contact"
+              onChange={changeHandler}
+              type="text"
+              className="form-control"
+            />
+          </td>
+        </tr>
+        <tr>
+          <th>Address</th>
+          <td>
+            <input
+              value={state.address}
+              name="address"
+              onChange={changeHandler}
+              type="text"
+              className="form-control"
+            />
+          </td>
+        </tr>
+        <tr>
+          <td colSpan="2">
+            <input
+              type="submit"
+              onClick={submitForm}
+              className="btn btn-dark"
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
+
+export default UpdateEmp;
